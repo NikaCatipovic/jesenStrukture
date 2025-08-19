6d8df4060ddea1ba2bb064d4150d2c7b4596a1fa
NikaCatipovic <ncatip00@fesb.hr>
2025-08-19 22:43:28 +0200

    Zapocela rjesavat, nije dovrseno

--- ISPITNI_ZAD_slucajniBrojevi/slucajniBrojevi.c
+++ ISPITNI_ZAD_slucajniBrojevi/slucajniBrojevi.c
@@ -1,3 +1,11 @@
+/*Napisati program koji generira slucajne brojeve u opsegu
+130-160. Ukoliko broj ne postoji u vezanoj listi dodaje se
+sortirano po vrijednosti, a ukoliko broj vec postoji u
+vezanoj listi samo se uvecava broj ponavljanja tog broja.
+Ispisati listu (vrijednost i broj ponavljanja).
+
+OCJENA 2:Pronaci broj/brojeve koji su se ponovili najmanje puta i ispisati ih.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -23,21 +31,35 @@ int main() {
 
 void addEnd(Number* p) {
 
+
+	int br = rand() % (130 - 160 + 1) + 130; 
+
+	Number* prev = p;
+	Number* curr = p->next;
+
+	while(curr!=NULL && curr->value <  br){
+		prev = curr;
+		curr = curr->next;
+	}
+	if (curr != NULL && curr->value == br) {
+		
+		curr->no_repeats = curr->no_repeats + 1;
+		return;
+	}
 	Number* q = malloc(sizeof(Number));
 	if (q == NULL) {
 		printf("Greska pri alokaciji memorije!\n");
 		exit(1);
 	}
-
-	Number* prev = p;
-	Number* curr = p->next;
-
-	while(curr!=NULL && curr->value >  prev->value)
-
+	q->value = br;
+	q->no_repeats = 1;
+	q->next = curr;
+	prev->next = q;
+	
 	
 	
 	
 	//q->next = NULL;
-	//q->value = rand() % (130 - 160 + 1) + 130;
+	
 
 }
