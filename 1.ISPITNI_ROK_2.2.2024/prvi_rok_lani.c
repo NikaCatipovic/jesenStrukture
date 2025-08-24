21cf7cab727121115b711bae985e23ffda4307f1
NikaCatipovic <ncatip00@fesb.hr>
2025-08-24 21:11:36 +0200

    Rjesen 1.ispitni rok 2024. i zapocet 3.ispitni rok 2025.

---
+++ 1.ISPITNI_ROK_2.2.2024/prvi_rok_lani.c
@@ -0,0 +1,178 @@
+/*Napisati program koji iz datoteke "Studenti.txt" cita 
+podatke o studentima i njihovim mticnim brojevima, te 
+podatke sprema u vezanu listu sortiranu po prezimenu i 
+imenu studenta.
+Iz datoteke "Predmet1.txt" procitati maticni broj studenta
+i ocjenu iz predmeta, te ih upisati u odgovarajuci element
+liste. Ispisati listu.
+U izradi zadatka moze se koristit struktura:
+#define MAX_NAME_LEN (32)
+struct _student;
+typedef struct _student* StudentP;
+typedef struct _student {
+	char firstName[MAX_NAME_LEN];
+	char lastName[MAX_NAME_LEN];
+	int idNumber;
+	int grade;
+	StudentP Next;
+}Student;
+OCJENA 2: Unijeti ime s tastature, te iz liste izbrisati sve
+one studente koji se tako zovu. Ispisati listu.
+OCJENA 3: Iz postojece liste kreirati novu listu studenata
+koji su polozili predmet. Pri tome se ne smije koristiti
+nova alokacija, vec se samo pomicu pokazivaci. Na kraju, u
+jednoj listi su studenti koji nisu polozili predmet, a u drugoj
+oni koji su ga polozili. Ispisati obje liste.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_NAME_LEN (32)
+
+struct _student;
+typedef struct _student* StudentP;
+typedef struct _student {
+	char firstName[MAX_NAME_LEN];
+	char lastName[MAX_NAME_LEN];
+	int idNumber;
+	int grade;
+	StudentP Next;
+}Student;
+
+void citanjeDatoteke(const char* file, Student* p);
+void sortiranaLista(Student* p, char* surname, char* name, int ID);
+void ispisListe(Student* p);
+void freeList(Student* p);
+void otvaranjePredmet(const char* dat,Student* p);
+void pretrazivanjeID(Student* p, int ID, int ocjena);
+void ocjena2(Student* p);
+
+
+int main() {
+
+	struct _student Head;
+	Head.Next = NULL;
+
+	const char* file = "Studenti.txt";
+	const char* dat = "Predmet1.txt";
+
+	citanjeDatoteke(file, &Head);
+	otvaranjePredmet(dat, &Head);
+	ispisListe(&Head);
+	printf("--------------------------\n");
+	ocjena2(&Head);
+	printf("NOVA LISTA: \n");
+	ispisListe(&Head);
+
+
+	return 0;
+}
+
+void citanjeDatoteke(const char* file, Student* p) {
+	FILE* fp = fopen(file, "r");
+	if (fp == NULL) {
+		printf("Datoteka nije otvorena!\n");
+		exit(1);
+	}
+	char name[MAX_NAME_LEN], surname[MAX_NAME_LEN];
+	int ID;
+	char buffer[255];
+	while (fgets(buffer, 255, fp) != NULL) {
+		sscanf(buffer, "%s %s %d", surname, name, &ID);
+		sortiranaLista(p, surname, name, ID);
+	}
+	fclose(fp);
+}
+
+void sortiranaLista(Student* p, char* surname, char* name, int ID) {
+	Student* prev = p;
+	Student* curr = p->Next;
+	while (curr != NULL && (strcmp(curr->lastName, surname) < 0 || strcmp(curr->lastName,surname)==0 && strcmp(curr->firstName,name)<0)){
+		prev = curr;
+		curr = curr->Next;
+	}
+	
+	Student* q = malloc(sizeof(Student));
+	if (q == NULL) {
+		printf("Alokacija nije uspjesna!\n");
+		exit(2);
+	}
+	strcpy(q->lastName, surname);
+	strcpy(q->firstName, name);
+	q->idNumber = ID;
+	prev->Next = q;
+	q->Next = curr;
+
+}
+
+void ispisListe(Student* p) {
+	Student* curr = p->Next;
+	while (curr != NULL) {
+		printf("%s %s %08d %d\n", curr->lastName, curr->firstName, curr->idNumber, curr->grade);
+		curr = curr->Next;
+	}
+	printf("\n");
+}
+
+void freeList(Student* p) {
+	Student* curr = p->Next;
+	Student* temp;
+	while (curr != NULL) {
+		temp = curr;
+		curr = curr->Next;
+		free(temp);
+	}
+}
+
+void otvaranjePredmet(const char* dat, Student* p) {
+	FILE* f = fopen(dat, "r");
+	if (f == NULL) {
+		printf("Datoteka nije otvorena!\n");
+		exit(3);
+	}
+	int ID, ocjena;
+	char buffer[255];
+	while (fgets(buffer, 255, f) != NULL) {
+		sscanf(buffer, "%d %d", &ID, &ocjena);
+		pretrazivanjeID(p, ID, ocjena);
+	}
+}
+
+void pretrazivanjeID(Student* p, int ID, int ocjena) {
+	Student* curr = p->Next;
+	while (curr != NULL) {
+		if (curr->idNumber == ID) {
+			curr->grade = ocjena;
+			return;
+		}
+		curr = curr->Next;
+	}
+
+}
+
+void ocjena2(Student* p) {
+	printf("Unesi ime koje zelis izbrisati: \n ");
+	char ime[MAX_NAME_LEN];
+	scanf("%s", ime);
+	Student* prev = p;
+	Student* curr = p->Next;
+	Student* temp;
+	while (curr != NULL) {
+		if (strcmp(curr->firstName, ime) == 0) {
+			printf("Odabrali ste %s %s\n", curr->lastName, curr->firstName);
+			temp = curr;
+			prev->Next = temp->Next;
+			free(temp);
+
+			return;
+		}
+		prev = curr;
+		curr = curr->Next;
+
+	}
+	printf("Odabrani student/ica ne postoji u listi!\n");
+	
+}
+	
---
+++ 3.SPITNI_ROK 8.7.2025/08.07.2025.c
@@ -0,0 +1,124 @@
+/*Napisati program koji iz datoteke "studenti.txt" cita prezime
+i ime studenta kao i njegov ID, te kreira vezanu listu sortiranu
+po prezimenu i imenu studenta.
+Nakon toga iz datoteka "predmet1.txt" i "predmet2.txt" se cita
+ID studenta, ocjena i datum polaganja predmeta. Procitane podatke
+je potrebno unijeti u postojecu vezanu listu. Ispisati listu.
+Student je definirana strukturom:
+--------------------------------
+#define MAX_NAME_LEN (32)
+struct _student;
+typedef struct _student* StudentP;
+typedef struct _student {
+	char ID[10];
+	char firstName[MAX_NAME_LEN];
+	char lastName[MAX_NAME_LEN];
+	int grade[2];
+	Date date[2];
+}Student;
+Datum je definiran strukturom:
+-----------------------------
+struct _date;
+typedef struct _date {
+	int year;
+	int month;
+	int day;
+} Date;
+
+OCJENA 2: Unijeti jedan datum i izbrisati iz liste sve one osobe
+koje su polozile bilo koji od predmeta na taj datum. Ispisati 
+novu vezanu listu.
+OCJENA 3: Pronaci za svaki predmet osobe koje su ga polozile na
+najraniji datum i prebaciti ih na kraj liste. Pri tome treba
+voditi racuna da je vise osoba polozilo svaki pojedini predmet
+na taj datum. Ispisati novu vezanu listu.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_NAME_LEN (32)
+
+struct _student;
+typedef struct _student* StudentP;
+typedef struct _student {
+	char ID[10];
+	char firstName[MAX_NAME_LEN];
+	char lastName[MAX_NAME_LEN];
+	int grade[2];
+	Date date[2];
+	StudentP next;  //ja dodala
+}Student;
+
+struct _date;
+typedef struct _date {
+	int year;
+	int month;
+	int day;
+} Date;
+
+void otvaranje(const char* dat, Student* p);
+void sortiraniStudenti(Student* p, char* prezime, char* ime, char* JMBS);
+void ispis(Student* p);
+
+int main() {
+
+	struct _student Head;
+	Head.next = NULL;
+
+	const char* dat = "studenti.txt";
+	otvaranje(dat, &Head);
+	ispis(&Head);
+
+	return 0;
+}
+
+void otvaranje(const char* dat, Student* p) {
+	FILE* fp = fopen(dat, "r");
+	if (fp == NULL) {
+		printf("Datoteka nije otvorena!\n");
+		exit(1);
+	}
+	char buffer[255];
+	char JMBS, ime, prezime;
+	while (fgets(buffer, 255, fp) != NULL) {
+		sscanf(buffer, "%s %s %s", prezime, ime, JMBS);
+		sortiraniStudenti(p, prezime, ime, JMBS);
+	}
+
+	fclose(fp);
+}
+
+void sortiraniStudenti(Student* p, char* prezime, char* ime, char* JMBS) {
+	Student* prev = p;
+	Student* curr = p->next;
+	
+	while (curr != NULL && strcmp(curr->lastName, prezime) < 0 && strcmp(curr->firstName, ime) < 0){
+
+		prev = curr;
+		curr = curr->next;
+
+	}
+
+	Student* q = malloc(sizeof(Student));
+	if (q == NULL) {
+		printf("Neuspjesna alokacija memorije\n");
+		exit(2);
+	}
+	strcpy(q->lastName, prezime);
+	strcpy(q->firstName, ime);
+	strcpy(q->ID, JMBS);
+	prev->next = q;
+	q->next = curr;
+
+}
+
+void ispis(Student* p) {
+	Student* curr = p->next;
+	while (curr != NULL) {
+		printf("%s %s %s\n", curr->lastName, curr->firstName, curr->ID);
+		curr = curr->next;
+	}
+	printf("\n");
+} 
